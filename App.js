5c4148024b7083afd0159588cf82d3bed515f0f7
neowresp <robinson_lemos@yahoo.com>
2022-06-16 11:49:58 -0300

    master

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, Image, PermissionsAndroid, TextComponent, Pressable, Linking } from 'react-native';
+
+const ImgProfile = "https://avatars.githubusercontent.com/u/49663041?v=4"
+const urlGit = "https://github.com/neowresp"
 
 export default function App() {
+
+  const github = async () => {
+    const re = await Linking.canOpenURL(urlGit);
+    if(re){
+      await Linking.openURL(urlGit)
+    }
+  }
+
   return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#010409" barStyle="light-content" />
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Kazuma</Text>
+      <Image accessibilityLabel='Kazuma' style={styles.avatar} source={{uri:ImgProfile}}/>
+      <Text style={styles.nickname}>@neowresp</Text>
+      <Text style={styles.description}>Uma simples pessoas querendo jogar seus joguinhos.</Text>
+      <StatusBar style="auto"/>
+      <Pressable onPress={github}>
+      <Text style={styles.button}>Github</Text>
+      </Pressable>
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#010409',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    padding: 20,
+    },
+    text:{
+      fontWeight: 'bold',
+      fontStyle: 'italic',
+      fontSize: 50,
+      color: "white",
+      padding: 20,
+    },
+    avatar: {
+      height: 150,
+      width: 150,
+      borderRadius: 100,
+      borderWidth: 5,
+      borderColor: "white",
+      padding: 20,
+    },
+    nickname: {
+      color: "grey",
+      padding: 15,
+    },
+    description: {
+      color: "white",
+      textAlign: "center",
+      fontSize: 20,
+    },
+    button: {
+      marginTop: 40,
+      backgroundColor: "grey",
+      borderRadius: 20,
+      padding: 20,
+      fontWeight: "bold",
+      fontSize: 20,
+      color: "black",
+    },
 });
